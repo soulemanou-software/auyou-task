4b9edbd2ff9f8310529fad5c5aaf16bc8c492ce2
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2024-05-21 01:40:03 +0100

    THIS IS JUST A TRANING FOR MY TODO APP USING SUPABASE AND REACT

--- src/configs/supabase.tsx
+++ src/configs/supabase.tsx
@@ -1,7 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+// Check that environment variables are defined
+if (!process.env.REACT_APP_SUPABASE_URL) {
+  throw new Error('Missing REACT_APP_SUPABASE_URL environment variable');
+}
+if (!process.env.REACT_APP_SUPABASE_ANON_KEY) {
+  throw new Error('Missing REACT_APP_SUPABASE_ANON_KEY environment variable');
+}
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
+
+// Create a Supabase client instance
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
